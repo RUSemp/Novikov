899d7dfe686799f4a3cadc5ef493daa9a9c1b2a0
RUSemp <44526428+RUSemp@users.noreply.github.com>
2018-10-28 06:04:49 +0400

    Update Game1/Game1/Program.cs
    complete game

--- Game1/Game1/Program.cs
+++ Game1/Game1/Program.cs
@@ -18,16 +18,15 @@ namespace Game1
                 Console.Write(num[i]);
             }
             Console.WriteLine();
-            string f = "да", result= "";
-            int number;
-            while ((f != "нет")||(result!= "Вы угадали число!"))
+            string result= "";
+            int number; 
+            while ((result != "Вы угадали число!"))
             {
                 Console.Write("Введите число: ");
                 number = Convert.ToInt32(Console.ReadLine());
                 result = trying.Check(number, num);
                 Console.WriteLine(result);
-                Console.WriteLine("Продолжить?");
-                f = Console.ReadLine();
+                
             }
 
 
