718049b42b19ecb0bfc16db92e4dbbbfbed3a9df
RUSemp <44526428+RUSemp@users.noreply.github.com>
2018-11-02 19:41:55 +0400

    BattleShip
    Version0.1

---
+++ BattleShip/ConsoleApp4/Pole2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeBatl
+{
+    public class Pole2
+    {
+        public string[,] pole2()
+        {
+            var Onep = new Onepalub();
+            const int one = 4;
+            const int two = 3;
+            const int three = 2;
+            const int fore = 1;
+            string[,] pole22 = new string[10, 10];
+            string onep = Onep.Onepalub1();
+            for (int i = 0; i<10;i++)
+            {
+                for (int j = 0; j<10;j++)
+                {
+                    pole22[i,j] = "+";
+                }
+            }
+            Console.Write("Введите координаты однопалубных кораблей: ");
+            int gor = Convert.ToInt32(Console.ReadLine());
+            int ver = Convert.ToInt32(Console.ReadLine());
+            for (int q = 0; q < one; q++)
+            {
+                for (int i = 0; i < pole22.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pole22.GetLength(1); j++)
+                    {
+                        if((gor == i)&&(ver == j))
+                        {
+                            Console.Write(onep);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.Write(pole22);
+            return pole22;
+        }
+    }
+}
---
+++ BattleShip/ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeBatl
+{
+    public class Game
+    {
+        static void Main(string[] args)
+        {
+            var Polee1 = new Pole1();
+            var Polee2 = new Pole2();
+            string[,] pole22 = Polee2.pole2();
+            for (int i = 0; i < pole22.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole22.GetLength(1); j++)
+                {
+                    Console.Write(pole22[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            string[,] pole11 = Polee1.pole1();
+            for (int i = 0; i < pole11.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole11.GetLength(1); j++)
+                {
+                    Console.Write(pole11[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
